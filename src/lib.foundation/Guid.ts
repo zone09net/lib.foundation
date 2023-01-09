23d7f7ab82d3c66a501d02fd3049de4a2fc054a6
zone09net <rcote@zone09.net>
2023-01-09 13:46:56 -0500

    Faster guid generation

--- src/lib.foundation/Guid.ts
+++ src/lib.foundation/Guid.ts
@@ -15,13 +15,27 @@ export class Guid
 			version = version ? (/1|2|3|4|5/.test(version) ? version : '1') : '1';
 			variant = variant ? (/8|9|a|b/.test(variant) ? variant : 'a') : 'a';
 
-			let str = 'xxxxxxxx-xxxx-' + version + 'xxx-' + variant + 'xxx-xxxxxxxxxxxx';
+			//let str = 'xxxxxxxx-xxxx-' + version + 'xxx-' + variant + 'xxx-xxxxxxxxxxxx';
 
 			do
 			{
+				/*
 				guid = str.replace(/[xy]/g, () => {
 					return (Math.random() * 16 | 0).toString(16);
 				});
+				*/
+				
+				let random: string = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
+				let splitted: Array<string> = random.substring(0, 36).split('');
+
+				splitted[8] = '-';
+				splitted[13] = '-';
+				splitted[14] = version
+				splitted[18] = '-';
+				splitted[19] = variant
+				splitted[23] = '-';
+
+				guid = splitted.join('');
 			}
 			while(this._guids.includes(guid))
 		}
